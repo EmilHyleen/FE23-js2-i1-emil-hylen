1e1eb3ca8d32a4806e2d78c03592b3e2712e0fca
Felix Svensson <felixsvensson1996@gmail.com>
2024-02-12 15:22:43 +0100

    Update Fighter.js
    Basic version

---
+++ modules/Fighter.js
@@ -0,0 +1,12 @@
+
+
+export default class Fighter {
+
+    constructor(fighterName, weapon, damage, health) {
+        this.fighterName = fighterName;
+        this.weapon = weapon;
+        this.damage = damage;
+        this.health = health;
+    }
+
+}
