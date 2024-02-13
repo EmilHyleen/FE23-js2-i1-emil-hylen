34bfb3ec40beb9f555a06936354f5d9b4d7f11de
Emil Hylen <111509606+EmilHyleen@users.noreply.github.com>
2024-02-13 13:53:58 +0100

    Fight Screen
    Fight screen non-oop base functionality and styling

---
+++ fightScreen.js
@@ -0,0 +1,121 @@
+//FIXME: This data needs to come from classes
+let p1Name = "Emil";
+let p1Health = 100;
+let p1AttackDmg = 25;
+
+let p2Name = "Felix";
+let p2Health = 100;
+let p2AttackDmg = 25;
+
+function UpdateDisplay()
+{
+    document.getElementById("p1HealthDisplay").innerText = 'Health: ' + p1Health;
+    document.getElementById("p2HealthDisplay").innerText = 'Health: ' + p2Health;
+    
+    document.getElementById("p1AttackBtn").innerText = 'Attack (' + p1AttackDmg + ')';
+    document.getElementById("p2AttackBtn").innerText = 'Attack (' + p2AttackDmg + ')';
+}
+
+function InitDisplay()
+{
+    document.getElementById("announcer").innerText = "Fight!";
+
+    document.getElementById("p1NameDisplay").innerText = p1Name;
+    document.getElementById("p2NameDisplay").innerText = p2Name;
+    
+    document.getElementById("p1AttackBtn").disabled = false;
+    document.getElementById("p1PowerUpBtn").disabled = false;
+    document.getElementById("p2AttackBtn").disabled = true;
+    document.getElementById("p2PowerUpBtn").disabled = true;
+
+    UpdateDisplay();
+}
+InitDisplay();
+
+function ResetGame()
+{
+    p1Health = 100;
+    p2Health = 100;
+
+    InitDisplay();
+}
+
+function ToggleTurn()
+{
+    if (document.getElementById("p2AttackBtn").disabled)
+    {
+        document.getElementById("p1AttackBtn").disabled = true;
+        document.getElementById("p1PowerUpBtn").disabled = true;
+        document.getElementById("p2AttackBtn").disabled = false;
+        document.getElementById("p2PowerUpBtn").disabled = false;
+    }
+    else
+    {
+        document.getElementById("p1AttackBtn").disabled = false;
+        document.getElementById("p1PowerUpBtn").disabled = false;
+        document.getElementById("p2AttackBtn").disabled = true;
+        document.getElementById("p2PowerUpBtn").disabled = true;
+    }
+}
+
+function DisableAll()
+{
+    document.getElementById("p1AttackBtn").disabled = true;
+    document.getElementById("p2AttackBtn").disabled = true;
+    document.getElementById("p1PowerUpBtn").disabled = true;
+    document.getElementById("p2PowerUpBtn").disabled = true;
+}
+
+function EvaluateWinner()
+{
+    if (p1Health <= 0 && p2Health > 0)
+    {
+        document.getElementById("announcer").innerText = "Player 2 Wins";
+        DisableAll();
+    }
+    else if (p2Health <= 0 && p1Health > 0)
+    {
+        document.getElementById("announcer").innerText = "Player 1 Wins";
+        DisableAll();
+    }
+    else if (p1Health <= 0 && p2Health <= 0)
+    {
+        document.getElementById("announcer").innerText = "Draw";
+        DisableAll();
+    }
+}
+
+//FIXME: All attacks/player actions needs to come from a class
+function P1Attack()
+{
+    p2Health -= p1AttackDmg;
+
+    UpdateDisplay();
+    ToggleTurn();
+    EvaluateWinner();
+}
+
+function P2Attack()
+{
+    p1Health -= p2AttackDmg;
+
+    UpdateDisplay();
+    ToggleTurn();
+    EvaluateWinner();
+}
+
+function P1PowerUp()
+{
+    p1AttackDmg += 5;
+    
+    UpdateDisplay();
+    ToggleTurn();
+}
+
+function P2PowerUp()
+{
+    p2AttackDmg += 5;
+
+    UpdateDisplay();
+    ToggleTurn();
+}
