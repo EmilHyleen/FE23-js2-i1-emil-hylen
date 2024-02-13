9a10dd5547656c1e2d84a07f69ead8e1379d31eb
Felix Svensson <felixsvensson1996@gmail.com>
2024-02-13 15:58:27 +0100

    Update Fighter.js
    added MoveButtons from Fighter.js

--- modules/Fighter.js
+++ modules/Fighter.js
@@ -1,14 +1,30 @@
 export default class Fighter {
 
-    constructor(name, health, moves) {
+    constructor(player1Or2, name, health, moves) {
+        this.player1Or2 = player1Or2;
         this.name = name;
         this.health = health;
         this.moves = moves;
+        addAttackButtons();
     }
 
     
-    TakeDamage(move) {
-        this.health -= damage;
+    takeDamage(lostDamage) {
+        this.health -= lostDamage;
+    }
+    
+    addMoveButtons() {
+        const container = document.querySelector('.p' + this.player1Or2 + 'MoveButtons');
+        Array.isArray(this.moves) 
+        ? this.moves.forEach(move => {
+            const button = document.createElement('button');
+            button.value = `${this.player1Or2}-${move.damage}`;
+            button.id = `${this.player1Or2}-${move.name}`;
+            button.innerText = move.name;
+            container.appendChild(button);
+        })
+        : '';
+        
     }
     
     logFighterSpecs(){
@@ -29,21 +45,24 @@ class Move {
 }
 
 export class Emil extends Fighter {
-    constructor() {
+    constructor(player1Or2) {
+        this.player1Or2 = player1Or2;
         const moves = [];
         
         moves.push(new Move('punch', 25));
         moves.push(new Move('kick', 15));
-        super('Emil', 100, moves);
+        super(player1Or2, 'Emil', 100, moves);
     }
 }
 
 export class Felix extends Fighter {
-    constructor() {
+    constructor(player1Or2) {
+        this.player1Or2 = player1Or2;
+
         const moves = [];
         
         moves.push(new Move('throw', 17));
         moves.push(new Move('step', 22));
-        super('Felix', 100, moves);
+        super(player1Or2, 'Felix', 100, moves);
     }
 }
