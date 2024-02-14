2169c2403771f476939d67269ca205ca558e1114
Emil Hylen <111509606+EmilHyleen@users.noreply.github.com>
2024-02-14 14:03:22 +0100

    Players can attack eachother

--- fightScreen.js
+++ fightScreen.js
@@ -10,13 +10,53 @@ felix.addMoveButtons();
 const p1ButtonsContainer = document.querySelector('.p1MoveButtons');
 const p2ButtonsContainer = document.querySelector('.p2MoveButtons');
 
-
-p1ButtonsContainer.addEventListener('click', logTest);
+p1ButtonsContainer.addEventListener('click', EvaluateDamage);
+p2ButtonsContainer.addEventListener('click', EvaluateDamage);
 
 function logTest() {
     console.log('This is a test');
 }
 
+function EvaluateDamage({target})
+{
+    const Data = target.value.split('-');
+    const AttackingPlayerID = parseInt(Data[0]);
+    const Damage = parseInt(Data[1]);
 
+    switch(AttackingPlayerID)
+    {
+        case 1:
+            felix.takeDamage(Damage);
+            break;
+        case 2:
+            emil.takeDamage(Damage);
+            break;
+        default:
+            console.log("Something went very wrong, PlayerID not recognized");
+    }
+target
+    UpdateDisplay();
+}
 
+function UpdateDisplay()
+{
+    const AnnouncerElem = document.getElementById("announcer");
+    if (emil.isDead())
+    {
+        AnnouncerElem.innerText = felix.name + " wins!";
+    }
+    else if (felix.isDead())
+    {
+        AnnouncerElem.innerText = emil.name + " wins!";
+    }
+    else
+    {
+        AnnouncerElem.innerText = "Fight!";
+    }
 
+    document.getElementById("p1NameDisplay").innerText = emil.name;
+    document.getElementById("p2NameDisplay").innerText = felix.name;
+    document.getElementById("p1HealthDisplay").innerText = 'Health: ' + emil.health;
+    document.getElementById("p2HealthDisplay").innerText = 'Health: ' + felix.health;
+}
+UpdateDisplay();
